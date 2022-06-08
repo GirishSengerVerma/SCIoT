af0747ef8bf36ef814131ff55bfb5f8dd1b36cd5
Tobias <senger.tobias@web.de>
2022-06-08 14:25:29 +0200

    Dashboard Web App: Integrating Prisma, global styles, layout and error (WIP)

---
+++ dashboard-web-app/src/routes/__error.svelte
@@ -0,0 +1,34 @@
+<script context="module" lang="ts">
+	import type { Load } from '@sveltejs/kit'
+
+	export const load: Load = ({ error, status }) => {
+		return {
+			props: {
+				title: `${status}: ${error?.message}`
+			}
+		}
+	}
+</script>
+
+<script lang="ts">
+	export let title: string
+</script>
+
+<div class="error">
+	<h1>{title}</h1>
+	<span class="emoji">⚠️</span>
+</div>
+
+<style>
+	.error {
+		height: 100vh;
+		display: grid;
+		gap: var(--spacing-32);
+		place-content: center;
+		place-items: center;
+	}
+
+    .emoji {
+        font-size: 20rem;
+    }
+</style>
