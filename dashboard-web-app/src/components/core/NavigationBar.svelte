e35c51f695dab5805e3c4c559ff08ed134f339e5
Tobias Senger <senger.tobias@web.de>
2022-06-25 13:55:49 +0200

    Dashboard Web App: Improved NavigationBar

--- dashboard-web-app/src/components/core/NavigationBar.svelte
+++ dashboard-web-app/src/components/core/NavigationBar.svelte
@@ -1,9 +1,6 @@
 <script lang="ts">
 import { NAVIGATION_ICON_ACTUATOR, NAVIGATION_ICON_AUTHORITY, NAVIGATION_ICON_EVENT, NAVIGATION_ICON_LOCATION, NAVIGATION_ICON_SENSOR, NAVIGATION_ICON_SETTINGS, NAVIGATION_ICON_SOCIAL_MEDIA, NAVIGATION_ICON_THEME_SWITCH } from "$root/constants/iconConstants";
 
-	// your script goes here
-
-import { LIGHT_MODE, theme } from "$root/stores/themeStore";
 import NavigationBarItem from "./NavigationBarItem.svelte";
 
 </script>
@@ -19,10 +16,11 @@ import NavigationBarItem from "./NavigationBarItem.svelte";
             rounded-2xl
             p-7"
 >
-    <div class="my-5">
-        <img src={$theme === LIGHT_MODE ? "logo/logo_lightmode.svg" : "logo/logo_darkmode.svg" } alt="DWA Logo" class="w-20"/>
-    </div>
-	<div>
+    <a class="my-5" href="/">
+        <img src="logo/logo_lightmode.svg" alt="DWA Logo" class="w-20 dark:hidden"/>
+        <img src="logo/logo_darkmode.svg" alt="DWA Logo" class="w-20 hidden dark:block"/>
+    </a>
+	<div class="my-5">
         <NavigationBarItem      iconName={NAVIGATION_ICON_EVENT}            iconAlt="Events"/>
         <NavigationBarItem      iconName={NAVIGATION_ICON_LOCATION}         iconAlt="Locations"/>
         <NavigationBarItem      iconName={NAVIGATION_ICON_SENSOR}           iconAlt="Sensors"/>
@@ -30,8 +28,8 @@ import NavigationBarItem from "./NavigationBarItem.svelte";
         <NavigationBarItem      iconName={NAVIGATION_ICON_AUTHORITY}        iconAlt="Authorities"/>
         <NavigationBarItem      iconName={NAVIGATION_ICON_SOCIAL_MEDIA}     iconAlt="Social Media"/>
     </div>
-    <div class="my-5">
-        <NavigationBarItem      iconName={NAVIGATION_ICON_THEME_SWITCH}     iconAlt="Switch Theme"  alwaysShowBorder/>
-        <NavigationBarItem      iconName={NAVIGATION_ICON_SETTINGS}         iconAlt="Settings"      alwaysShowBorder/>
+    <div>
+        <NavigationBarItem      iconName={NAVIGATION_ICON_THEME_SWITCH}     iconAlt="Switch Theme"  options/>
+        <NavigationBarItem      iconName={NAVIGATION_ICON_SETTINGS}         iconAlt="Settings"      options/>
     </div>
 </div>
--- dashboard-web-app/src/components/core/NavigationBarItem.svelte
+++ dashboard-web-app/src/components/core/NavigationBarItem.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
     export let iconName: string;
     export let iconAlt: string;
-    export let alwaysShowBorder: boolean = false;
+    export let options: boolean = false;
 </script>
 
-<div class={(alwaysShowBorder 
-        ? "border rounded-lg border-accentLight dark:border-accentDark" 
-        : "hover:border hover:rounded-lg hover:border-accentLight dark:hover:border-accentDark") 
-        + " p-4 my-4 hover:cursor-pointer"}>
-    <img src={"icons/nav/" + iconName + ".svg"} alt={iconAlt} class="w-7"/>
+<div class={
+    "hover:cursor-pointer transition border rounded-lg" 
+    + (options ? 
+        " my-2 p-2 border-accentLight dark:border-accentDark hover:bg-accentLight/[.15] dark:hover:bg-accentDark/[.25]" : 
+        " my-4 p-4 rounded-lg border-transparent  hover:border hover:rounded-lg hover:border-accentLight dark:hover:border-accentDark")
+}>
+    <img src={"icons/nav/" + iconName + ".svg"} alt={iconAlt} class={(options ? "w-6" : "w-7") + ' dark:invert'}/>
 </div>
