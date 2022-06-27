dea6f2b63919ccb2e4755fcf11278f7c46d076ea
Tobias Senger <senger.tobias@web.de>
2022-06-27 12:18:50 +0200

    Dashboard Web App: Updated icon paths for NavigationBar[Item]

--- dashboard-web-app/src/components/core/NavigationBar.svelte
+++ dashboard-web-app/src/components/core/NavigationBar.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { NAVIGATION_ICON_ACTUATOR, NAVIGATION_ICON_AUTHORITY, NAVIGATION_ICON_EVENT, NAVIGATION_ICON_LOCATION, NAVIGATION_ICON_SENSOR, NAVIGATION_ICON_SETTINGS, NAVIGATION_ICON_SOCIAL_MEDIA, NAVIGATION_ICON_THEME_SWITCH } from "$root/constants/iconConstants";
+    import { ICON_COMMON_ACTUATOR, ICON_COMMON_AUTHORITY, ICON_COMMON_EVENT, ICON_COMMON_LOCATION, ICON_COMMON_SENSOR, ICON_COMMON_SETTINGS, ICON_COMMON_SOCIAL_MEDIA, ICON_COMMON_THEME_SWITCH } from "$root/constants/iconConstants";
     import { theme } from "$root/stores/themeStore";
 
     import NavigationBarItem from "$root/components/core/NavigationBarItem.svelte";
@@ -27,19 +27,19 @@
         <img src="logo/logo_darkmode.svg" alt="DWA Logo" class="w-20 hidden dark:block"/>
     </a>
 	<div class="flex md:block w-full md:my-5 items-center justify-evenly">
-        <NavigationBarItem      iconName={NAVIGATION_ICON_EVENT}            iconAlt="Events"        link="/events"      selected={route === "events"}       />
-        <NavigationBarItem      iconName={NAVIGATION_ICON_LOCATION}         iconAlt="Locations"     link="/locations"   selected={route === "locations"}    />
-        <NavigationBarItem      iconName={NAVIGATION_ICON_SENSOR}           iconAlt="Sensors"       link="/sensors"     selected={route === "sensors"}      />
+        <NavigationBarItem      iconName={ICON_COMMON_EVENT}            iconAlt="Events"        link="/events"      selected={route === "events"}       />
+        <NavigationBarItem      iconName={ICON_COMMON_LOCATION}         iconAlt="Locations"     link="/locations"   selected={route === "locations"}    />
+        <NavigationBarItem      iconName={ICON_COMMON_SENSOR}           iconAlt="Sensors"       link="/sensors"     selected={route === "sensors"}      />
         <div class="md:hidden border rounded-lg">
-            <NavigationBarItem      iconName={NAVIGATION_ICON_THEME_SWITCH}     iconAlt="Switch Theme"  options    link={route}     onClick={() => theme.switch()}  />
-            <NavigationBarItem      iconName={NAVIGATION_ICON_SETTINGS}         iconAlt="Settings"      options    link="/settings" selected={route === "settings"} />
+            <NavigationBarItem      iconName={ICON_COMMON_THEME_SWITCH}     iconAlt="Switch Theme"  options    link={route}     onClick={() => theme.switch()}  />
+            <NavigationBarItem      iconName={ICON_COMMON_SETTINGS}         iconAlt="Settings"      options    link="/settings" selected={route === "settings"} />
         </div>
-        <NavigationBarItem      iconName={NAVIGATION_ICON_ACTUATOR}         iconAlt="Actuators"     link="/actuators"   selected={route === "actuators"}        />
-        <NavigationBarItem      iconName={NAVIGATION_ICON_AUTHORITY}        iconAlt="Authorities"   link="/authorities" selected={route === "authorities"}      />
-        <NavigationBarItem      iconName={NAVIGATION_ICON_SOCIAL_MEDIA}     iconAlt="Social Media"  link="/socialmedia" selected={route === "socialmedia"}      />
+        <NavigationBarItem      iconName={ICON_COMMON_ACTUATOR}         iconAlt="Actuators"     link="/actuators"   selected={route === "actuators"}        />
+        <NavigationBarItem      iconName={ICON_COMMON_AUTHORITY}        iconAlt="Authorities"   link="/authorities" selected={route === "authorities"}      />
+        <NavigationBarItem      iconName={ICON_COMMON_SOCIAL_MEDIA}     iconAlt="Social Media"  link="/socialmedia" selected={route === "socialmedia"}      />
     </div>
     <div class="hidden md:block px-2 md:p-5">
-        <NavigationBarItem      iconName={NAVIGATION_ICON_THEME_SWITCH}     iconAlt="Switch Theme"  options     link={route}        onClick={() => theme.switch()}  />
-        <NavigationBarItem      iconName={NAVIGATION_ICON_SETTINGS}         iconAlt="Settings"      options     link="/settings"    selected={route === "settings"} />
+        <NavigationBarItem      iconName={ICON_COMMON_THEME_SWITCH}     iconAlt="Switch Theme"  options     link={route}        onClick={() => theme.switch()}  />
+        <NavigationBarItem      iconName={ICON_COMMON_SETTINGS}         iconAlt="Settings"      options     link="/settings"    selected={route === "settings"} />
     </div>
 </div>
--- dashboard-web-app/src/components/core/NavigationBarItem.svelte
+++ dashboard-web-app/src/components/core/NavigationBarItem.svelte
@@ -19,7 +19,7 @@
                     " p-1 md:my-2 md:p-2 border-transparent md:border-accentLight md:dark:border-accentDark hover:bg-accentLight/[.20] dark:hover:bg-accentDark/[.25]" : 
                     " p-2 md:my-4 md:p-4 rounded-lg border-transparent  hover:border hover:rounded-lg hover:border-accentLight dark:hover:border-accentDark")
             }>
-                <img src={"icons/nav/" + iconName + ".svg"} alt={iconAlt} class={(options ? "w-5 md:w-6" : "w-6 md:w-7") + " dark:invert max-w-none"}/>
+                <img src={"icons/" + iconName + ".svg"} alt={iconAlt} class={(options ? "w-5 md:w-6" : "w-6 md:w-7") + " dark:invert max-w-none"}/>
             </div>
         </a>
     </NavigationBarSelectedItemIndicator>
